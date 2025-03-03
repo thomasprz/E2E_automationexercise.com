ba7230889628501ed77726223ce9393a6525dabd
Thomas <przegralek.thomas@gmail.com>
2025-03-03 22:21:11 +0100

    Ajout de la page d'accueil

---
+++ e2e/pages/home.page.ts
@@ -0,0 +1,10 @@
+import {Page, Locator, expect} from '@playwright/test'
+import { BasePage } from './base.page'
+
+export class HomePage extends BasePage {
+
+
+    constructor(page:Page){
+        super(page, '/')
+    }
+}
