39b22e85e6ca8cf0e633bf27f88916a69fb1ff82
Thomas <przegralek.thomas@gmail.com>
2025-03-04 21:29:53 +0100

    Mise à jour de la page d'accueil

--- e2e/pages/home.page.ts
+++ e2e/pages/home.page.ts
@@ -2,9 +2,15 @@ import {Page, Locator, expect} from '@playwright/test'
 import { BasePage } from './base.page'
 
 export class HomePage extends BasePage {
-
+    readonly locatorHomepageHeader : Locator
 
     constructor(page:Page){
         super(page, '/')
+        this.locatorHomepageHeader = page.getByAltText('Website for automation practice');
+    }
+
+    async expectHomepage(){
+        await expect(this.page).toHaveURL(this.url);
+        await expect(this.locatorHomepageHeader).toBeVisible()
     }
 }
