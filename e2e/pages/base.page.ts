bcce5cfd0dacb539df7b64cbf37506724c2e9933
Thomas <przegralek.thomas@gmail.com>
2025-03-04 21:29:46 +0100

    Modification de la page de base pour améliorer la réutilisabilité

--- e2e/pages/base.page.ts
+++ e2e/pages/base.page.ts
@@ -3,14 +3,20 @@ import {Page, Locator} from '@playwright/test'
 export class BasePage {
     readonly page : Page 
     readonly url : string
+    readonly locatorPopup : Locator
 
     constructor(page:Page, url:string){
         this.page = page
         this.url = url
-    }
+        this.locatorPopup = page.getByRole("button", {name:'Consent'});
+      }
 
   async goTo() {
     await this.page.goto(this.url);
     await this.page.waitForLoadState('networkidle')
+  }
+
+  async popup(){
+    await this.locatorPopup.click()
   }
 }
