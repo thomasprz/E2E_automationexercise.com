a3a4c7be8ed843c1051d7cb987040012b9d766ef
Thomas <przegralek.thomas@gmail.com>
2025-03-05 20:51:32 +0100

    Mise à jour des fixtures pour inclure les nouvelles pages

--- e2e/fixtures/pages.fixture.ts
+++ e2e/fixtures/pages.fixture.ts
@@ -2,11 +2,13 @@ import {test as pagesTest} from '@playwright/test'
 import { HomePage } from '../pages/home.page'
 import { LoginPage } from '../pages/login.page'
 import { SignupPage } from '../pages/signup.page'
+import { RequestApiPage } from '../pages/api/api.page'
 
 interface Pages {
     home : HomePage,
     login : LoginPage,
     signup : SignupPage,
+    api: RequestApiPage
 }
 
 export const pages = pagesTest.extend<Pages>({
@@ -18,5 +20,8 @@ export const pages = pagesTest.extend<Pages>({
     },
     signup: async ({page}, use) => {
         await use(new SignupPage(page))
-    }
+    },
+    api: async ({request}, use) => {
+        await use(new RequestApiPage(request))
+    },
 })
