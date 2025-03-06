4bd6edf520c8b30fea0eb8d3db894cce87a11b28
Thomas <przegralek.thomas@gmail.com>
2025-03-06 22:07:07 +0100

    Configuration de Playwright mise à jour pour les nouveaux tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,9 @@
 import { defineConfig, devices } from '@playwright/test';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, `.env`) });
 
 export default defineConfig({
   testDir: './e2e/tests',
@@ -11,14 +16,17 @@ export default defineConfig({
     baseURL: 'https://automationexercise.com',
     trace: 'on-first-retry',
     testIdAttribute: 'data-qa',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
   },
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: 'Chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
-
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
