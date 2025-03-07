4bd7f2a5487a7589d684c57d139833b76c3085b8
Thomas <przegralek.thomas@gmail.com>
2025-03-07 21:53:22 +0100

    Ajout du lien de navigation vers la page produits

--- e2e/components/menu.component.ts
+++ e2e/components/menu.component.ts
@@ -5,47 +5,50 @@ import { HomePage } from '../pages/home.page'
 
 export class MenuComponent {
     readonly page : Page
-    readonly home : Locator
-    readonly products : Locator
-    readonly cart : Locator
-    readonly logout : Locator
-    readonly deleteAccount : Locator
-    readonly signupLogin : Locator
-    readonly testCases : Locator
-    readonly apiTesting : Locator
-    readonly videoTutorials : Locator
-    readonly contactUs : Locator
+    readonly locatorhome : Locator
+    readonly locatorproducts : Locator
+    readonly locatorcart : Locator
+    readonly locatorlogout : Locator
+    readonly locatordeleteAccount : Locator
+    readonly locatorsignupLogin : Locator
+    readonly locatortestCases : Locator
+    readonly locatorapiTesting : Locator
+    readonly locatorvideoTutorials : Locator
+    readonly locatorcontactUs : Locator
 
     constructor(page:Page){
         this.page = page // MenuComponent n'a pas besoin d'utiliser les fonctionnalités de BasePage (comme la navigation vers une URL), donc supprimer l'appel à super(page) et ne pas hériter de BasePage.
-        this.home = page.getByRole('link', {name:'Home'})
-        this.signupLogin = page.getByRole('link', {name: ' Signup / Login'})
-        this.deleteAccount = page.getByRole('link', {name:' Delete Account'})
-        this.logout = page.getByRole('link', {name:' Logout'})
-        this.contactUs = page.getByRole('link', {name:' Contact us'})
+        this.locatorhome = page.getByRole('link', {name:'Home'})
+        this.locatorproducts = page.getByRole('link', {name: ' Products'})
+        this.locatorsignupLogin = page.getByRole('link', {name: ' Signup / Login'})
+        this.locatorlogout = page.getByRole('link', {name:' Logout'})
+        this.locatordeleteAccount = page.getByRole('link', {name:' Delete Account'})
+        this.locatortestCases = page.getByRole('link', {name:' Test Cases',  exact: true})
+        this.locatorcontactUs = page.getByRole('link', {name:' Contact us'})
     }
 
     async visitHome(){
-        await this.home.click()
+        await this.locatorhome.click()
     }
-
+    async visitProducts(){
+        await this.locatorproducts.click()
+    }
     async visitSignupLogin(){
-        await this.signupLogin.click()
+        await this.locatorsignupLogin.click()
     }
-
     async expectLoggedIn(username){
         await expect(this.page.getByRole('listitem').filter({ hasText: `Logged in as ${username}`})).toBeVisible()
     }
-
     async clickDeleteAccount(){
-        await this.deleteAccount.click()
+        await this.locatordeleteAccount.click()
     }
-
     async clickLogout(){
-        await this.logout.click()
+        await this.locatorlogout.click()
     }
-
     async visitContactUs(){
-        await this.contactUs.click()
+        await this.locatorcontactUs.click()
+    }
+    async visitTestCases(){
+        await this.locatortestCases.click()
     }
 }
