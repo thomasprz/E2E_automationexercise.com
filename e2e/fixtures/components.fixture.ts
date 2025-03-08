b2e7f59319f07cd2d7517290e11dd1165ddf7779
Thomas <przegralek.thomas@gmail.com>
2025-03-08 21:51:47 +0100

    Mise à jour des fixtures des composants

--- e2e/fixtures/components.fixture.ts
+++ e2e/fixtures/components.fixture.ts
@@ -1,12 +1,17 @@
+import { FooterComponent } from '../components/footer.component';
 import { MenuComponent } from '../components/menu.component';
 import {test as componentsTest} from '@playwright/test' // Importe l'objet `test` de Playwright, et la renomme en `componentsTest` indiquant que cet objet sera utilisé avec des fixtures personnalisées.
 
 interface Components {
     menu : MenuComponent, //Fixture appelée `menu`, qui est une instance de `MenuComponent`.
+    footer : FooterComponent
 }
 
 export const components = componentsTest.extend<Components>({
     menu: async({page}, use) => {
         await use(new MenuComponent(page))
-    }
+    },
+    footer : async({page}, use) => {
+        await use(new FooterComponent(page))
+    },
 })
