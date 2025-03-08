40f7473207f0e9cc9d6412d20f2b2efd14d207b5
Thomas <przegralek.thomas@gmail.com>
2025-03-08 21:52:16 +0100

    Mise à jour de la page d'accueil

--- e2e/pages/home.page.ts
+++ e2e/pages/home.page.ts
@@ -1,12 +1,17 @@
 import {Page, Locator, expect} from '@playwright/test'
 import { BasePage } from './base.page'
+import { FooterComponent } from '../components/footer.component';
 
 export class HomePage extends BasePage {
     readonly locatorHomepageHeader : Locator
+    //PAGE
+    readonly footer : FooterComponent
 
     constructor(page:Page){
         super(page)
         this.locatorHomepageHeader = page.getByAltText('Website for automation practice');
+        //PAGE
+        this.footer = new FooterComponent(page)
     }
 
     async expectHomepage(){
