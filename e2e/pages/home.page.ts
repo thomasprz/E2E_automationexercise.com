69ade9b6f1a81de0156c9251e40e37c43382ad53
Thomas <przegralek.thomas@gmail.com>
2025-03-12 21:50:33 +0100

    Mise à jour de home.page.ts pour l'affichage de la page d'accueil

--- e2e/pages/home.page.ts
+++ e2e/pages/home.page.ts
@@ -1,17 +1,20 @@
 import {Page, Locator, expect} from '@playwright/test'
 import { BasePage } from './base.page'
 import { FooterComponent } from '../components/footer.component';
+import { ProductsPage } from './products.page';
 
 export class HomePage extends BasePage {
     readonly locatorHomepageHeader : Locator
     //PAGE
     readonly footer : FooterComponent
+    readonly products : ProductsPage
 
     constructor(page:Page){
         super(page)
         this.locatorHomepageHeader = page.getByAltText('Website for automation practice');
         //PAGE
         this.footer = new FooterComponent(page)
+        this.products = new ProductsPage(page)
     }
 
     async expectHomepage(){
