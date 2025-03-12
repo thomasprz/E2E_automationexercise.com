2e15e142e6fb48bc1cfc6757819c41ebb8d503c8
Thomas <przegralek.thomas@gmail.com>
2025-03-12 21:50:07 +0100

    Amélioration de cart.page.ts pour gérer le panier utilisateur

--- e2e/pages/cart.page.ts
+++ e2e/pages/cart.page.ts
@@ -10,6 +10,10 @@ export class CartPage extends BasePage {
     readonly locatorCartTotal : Locator
     readonly locatorCartDelete : Locator
     readonly locatorProduct : Locator
+    readonly locatorProceedToCheckout : Locator
+    readonly locatorRegisterLogin : Locator
+    readonly locatorContinueOnCart : Locator
+    readonly locatorEmptyCart : Locator
 
     constructor(page:Page){
         super(page)
@@ -18,7 +22,11 @@ export class CartPage extends BasePage {
         this.locatorCartPrice = page.locator('.cart_price')
         this.locatorCartQuantity = page.locator('.cart_quantity')
         this.locatorCartTotal = page.locator('.cart_total')
-        this.locatorCartDelete = page.locator('.cart_delete')
+        this.locatorCartDelete = page.locator('.cart_quantity_delete')
+        this.locatorProceedToCheckout = page.locator('.check_out')
+        this.locatorRegisterLogin = page.getByRole('link', {name:'Register / Login'})
+        this.locatorContinueOnCart = page.getByRole('button', {name:'Continue On Cart'})
+        this.locatorEmptyCart = page.locator('#empty_cart')
         //PAGE
         this.footer = new FooterComponent(page)
     }
@@ -42,4 +50,34 @@ export class CartPage extends BasePage {
             await expect(this.page.getByRole('row', { name: product.name }).locator(this.locatorCartQuantity)).toHaveText(product.quantity);
             await expect(this.page.getByRole('row', { name: product.name }).locator(this.locatorCartTotal)).toHaveText(`Rs. ${totalPrice}`);
     }
+
+    async clickProceedToCheckoutAndContinue(){
+        await this.locatorProceedToCheckout.click()
+        await this.locatorContinueOnCart.click()
+    }
+
+    async clickProceedToCheckoutAndLogin(){
+        await this.locatorProceedToCheckout.click()
+        await this.locatorRegisterLogin.click()
+    }
+
+    async clickProceedToCheckout(){
+        await this.locatorProceedToCheckout.click()
+    }
+
+    async removeOneProduct(){
+        await this.locatorCartDelete.click()
+    }
+
+    async removeOneProductExpectOneProductLessInCart(product){
+        const products = await this.locatorProduct.count()
+        await this.locatorCartDelete.first().click()
+        await expect(this.locatorProduct).toHaveCount(products-1)
+    }
+
+    async expectEmptyCart(){
+        await expect(this.locatorEmptyCart).toBeVisible()
+        await expect(this.locatorEmptyCart).toContainText('Cart is empty!')
+    }
+
 }
