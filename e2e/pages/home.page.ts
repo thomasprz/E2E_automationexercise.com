4c5e0e28a9edb1ca28acec4655e092d08226926c
Thomas <przegralek.thomas@gmail.com>
2025-03-14 16:38:19 +0100

    Ajout des méthodes dans home.page.ts pour le test 'Ajouter au panier à partir des articles recommandés'

--- e2e/pages/home.page.ts
+++ e2e/pages/home.page.ts
@@ -6,6 +6,10 @@ import { MenuComponent } from '../components/menu.component';
 
 export class HomePage extends BasePage {
     readonly locatorHomepageHeader : Locator
+    readonly locatorRecommendedItemsHeader : Locator
+    readonly locatorAddToCart : Locator
+    readonly locatorContinueShoppingButton : Locator
+    readonly locatorViewCartLink : Locator
     //PAGE
     readonly footer : FooterComponent
     readonly products : ProductsPage
@@ -14,6 +18,10 @@ export class HomePage extends BasePage {
     constructor(page:Page){
         super(page)
         this.locatorHomepageHeader = page.getByAltText('Website for automation practice');
+        this.locatorRecommendedItemsHeader = page.getByRole('heading', {name:'recommended items'})
+        this.locatorAddToCart = page.locator('.recommended_items .add-to-cart')
+        this.locatorContinueShoppingButton = page.getByRole('button', {name:'Continue Shopping'})
+        this.locatorViewCartLink =page.getByRole('link', {name:'View Cart'})
         //PAGE
         this.footer = new FooterComponent(page)
         this.products = new ProductsPage(page)
@@ -23,5 +31,10 @@ export class HomePage extends BasePage {
     async expectHomepage(){
         await expect(this.page).toHaveURL('/');
         await expect(this.locatorHomepageHeader).toBeVisible()
+    }
+
+    async addRecommendedProductToCart(){
+        await this.locatorAddToCart.first().click()
+        await this.locatorViewCartLink.click()
     }
 }
