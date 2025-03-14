e21c03524636d0644094c1382c76523e6e827d46
Thomas <przegralek.thomas@gmail.com>
2025-03-14 21:02:48 +0100

    Mise à jour des données avec Faker pour mobile_number et address1

--- e2e/factories/signup.factory.ts
+++ e2e/factories/signup.factory.ts
@@ -14,13 +14,12 @@ export function createAccountAddressInformationFaker() {
     firstname: faker.person.firstName(),
     lastname: faker.person.lastName(),
     company: faker.company.name(),
-    address: faker.location.streetAddress(),
+    address1: faker.location.streetAddress(),
     address2: faker.location.secondaryAddress(),
     country: 'New Zealand',
     state: faker.location.state(),
     city: faker.location.city(),
     zipcode: faker.location.zipCode(),
-    mobile: faker.phone.number()
+    mobile_number: faker.phone.number()
   };
 }
-
